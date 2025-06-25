ef141301c693cf9f81efd71651042f9276dde4f7
shahid <shahidsayyed0212@gmail.com>
2025-06-25 22:37:28 +0530

    ././

--- frontend/src/components/configs/API/index.ts
+++ frontend/src/components/configs/API/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
  
 
 const API = axios.create({timeout: 300000,
-  baseURL: "https://hospital-managment-dynamic-web-app.onrender.com/api/",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   }
